3f695b6070b555792b7cd93e9b21ad9754d35ea8
Carlos Enrique Arango Saenz <43822218+22CEAS@users.noreply.github.com>
2020-11-20 17:39:33 -0500

    Se modifico

--- SGA_PCR/Vistas/Program.cs
+++ SGA_PCR/Vistas/Program.cs
@@ -16,8 +16,8 @@ namespace Apolo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new frmLogueo());
-            Application.Run(new frmProcesoSubirFacturas());
+            Application.Run(new frmLogueo());
+            //Application.Run(new frmProcesoSubirFacturas());
         }
     }
 }
